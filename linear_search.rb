716bc76774c43a7066ed9c74781627d72b27c72a
codeamac09 <codeamac09@gmail.com>
2016-03-30 13:23:36 -0600

    encuentra el index

--- linear_search.rb
+++ linear_search.rb
@@ -1,2 +1,16 @@
-def linear_search()
+def linear_search(num, array)
+  i = 0
+  while i < array.count 
+    if  array[i] == num 
+      return i
+    end
+    i += 1
+  end
 end
+
+ random_numbers = [ 4, 3, 2, 20, 5, 64, 78, 11, 43 ]
+
+p linear_search(20, random_numbers)
+
+p linear_search(29, random_numbers)
+
